451c9906065e79643176a95ed871e7215db71258
Adriano Boccardo Pochettino <adriano.boccardo@gmail.com>
2020-12-03 20:21:06 +0000

    Init + Day 1

---
+++ adventofcode2020/adventofcode2020/Day1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace adventofcode2020
+{
+    class Day1
+    {
+        public static string Part1()
+        {
+            List<int> reportData = new List<int>(File.ReadAllLines(@"Media/day1input.txt").Select(row => int.Parse(row)));
+
+            for (int i = 0; i < reportData.Count; i++)
+            {
+                for (int j = 1; j < reportData.Count; j++)
+                {
+                    if (reportData[i] + reportData[j] == 2020)
+                        return $"{reportData[i] * reportData[j]}";
+                }
+            }
+            throw new Exception("Flag not found");
+        }
+
+        public static string Part2()
+        {
+            List<int> reportData = new List<int>(File.ReadAllLines(@"Media/day1input.txt").Select(row => int.Parse(row)));
+
+            for (int i = 0; i < reportData.Count; i++)
+            {
+                for (int j = 1; j < reportData.Count; j++)
+                {
+                    for (int k = 2; k <reportData.Count; k++)
+                    {
+                        if (reportData[i] + reportData[j] + reportData[k] == 2020)
+                            return $"{reportData[i] * reportData[j] * reportData[k]}";
+                    }
+                }
+            }
+            throw new Exception("Flag not found");
+        }
+
+    }
+}
---
+++ adventofcode2020/adventofcode2020/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace adventofcode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Day 1 flags: {Day1.Part1()} & {Day1.Part2()}");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
