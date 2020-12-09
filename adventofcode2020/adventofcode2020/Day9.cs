c80d7d8ddeb5ad1108130219df0d203055f919c7
Adriano Boccardo Pochettino <adriano.boccardo@gmail.com>
2020-12-09 13:13:56 +0000

    Day 9

---
+++ adventofcode2020/adventofcode2020/Day9.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace adventofcode2020
+{
+    class Day9
+    {
+        public static string Part1()
+        {
+            List<long> numbers = File.ReadAllLines(@"Media/day9input.txt").Select(num => long.Parse(num)).ToList();
+
+            int preambleLength = 25;
+
+            for (int i = preambleLength; i < numbers.Count; i++)
+            {
+                if (!IsSum(numbers.Skip(i - preambleLength).Take(preambleLength).ToList(), numbers[i]))
+                {
+                    return $"{numbers[i]}";
+                }
+            }
+
+            return "Not found";
+        }
+
+        static bool IsSum(List<long> valuesToCheck, long sum)
+        {
+            for (int i = 0; i < valuesToCheck.Count - 1; i++)
+            {
+                for (int j = 1; j < valuesToCheck.Count; j++)
+                {
+                    if (valuesToCheck[i] + valuesToCheck[j] == sum)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        public static string Part2()
+        {
+            List<long> numbers = File.ReadAllLines(@"Media/day9input.txt").Select(num => long.Parse(num)).ToList();
+
+            if (!long.TryParse(Part1(), out long invalidNumber))
+                return "Not found";
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                List<long> currentNumbers = new List<long>();
+
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    currentNumbers.Add(numbers[j]);
+
+                    if (currentNumbers.Sum() == invalidNumber)
+                    {
+                        return $"{currentNumbers.Min() + currentNumbers.Max()}";
+                    }
+                    else if (currentNumbers.Sum() > invalidNumber)
+                        break;
+                }
+            }
+
+            return $"Not found";
+        }
+    }
+}
--- adventofcode2020/adventofcode2020/Program.cs
+++ adventofcode2020/adventofcode2020/Program.cs
@@ -6,14 +6,15 @@ namespace adventofcode2020
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"Day 1 flags: {Day1.Part1()} & {Day1.Part2()}");
-            Console.WriteLine($"Day 2 flags: {Day2.Part1()} & {Day2.Part2()}");
-            Console.WriteLine($"Day 3 flags: {Day3.Part1()} & {Day3.Part2()}");
-            Console.WriteLine($"Day 4 flags: {Day4.Part1()} & {Day4.Part2()}");
-            Console.WriteLine($"Day 5 flags: {Day5.Part1()} & {Day5.Part2()}");
-            Console.WriteLine($"Day 6 flags: {Day6.Part1()} & {Day6.Part2()}");
-            Console.WriteLine($"Day 7 flags: {Day7.Part1()} & {Day7.Part2()}");
-            Console.WriteLine($"Day 8 flags: {Day8.Part1()} & {Day8.Part2()}");
+            //Console.WriteLine($"Day 1 flags: {Day1.Part1()} & {Day1.Part2()}");
+            //Console.WriteLine($"Day 2 flags: {Day2.Part1()} & {Day2.Part2()}");
+            //Console.WriteLine($"Day 3 flags: {Day3.Part1()} & {Day3.Part2()}");
+            //Console.WriteLine($"Day 4 flags: {Day4.Part1()} & {Day4.Part2()}");
+            //Console.WriteLine($"Day 5 flags: {Day5.Part1()} & {Day5.Part2()}");
+            //Console.WriteLine($"Day 6 flags: {Day6.Part1()} & {Day6.Part2()}");
+            //Console.WriteLine($"Day 7 flags: {Day7.Part1()} & {Day7.Part2()}");
+            //Console.WriteLine($"Day 8 flags: {Day8.Part1()} & {Day8.Part2()}");
+            Console.WriteLine($"Day 9 flags: {Day9.Part1()} & {Day9.Part2()}");
 
 
 
